f4cabaca670cf6255fcb98ed6c2a959368c63b10
YousufMeme <75252991+YousufMeme@users.noreply.github.com>
2020-11-30 17:13:17 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -3,7 +3,7 @@ void main()
 {
 printf("Program to print all the alphabets from a to z.\n");
 char c;
-printf("Letters from a to z are: ");
+printf("Letters from a t o z are: ");
 for(c=97;c<=122;c++)    //Since the Ascii values of a to z lies in the range 97 to 122
 printf("%c ",c);
 getch();
